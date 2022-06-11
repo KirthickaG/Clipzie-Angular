8bc811b862d0ee23cc22c862ad2fd687f892ac15
KirthickaG <kirthickaguru@gmail.com>
2022-06-11 16:05:26 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCcr8yAuC9GFqKBim_8TnEO6NygK37NjrI",
+    authDomain: "clipzie-ec933.firebaseapp.com",
+    projectId: "clipzie-ec933",
+    storageBucket: "clipzie-ec933.appspot.com",
+    messagingSenderId: "597888306024",
+    appId: "1:597888306024:web:bbdf3dc750fb642a4be4dd"
+  }
 };
